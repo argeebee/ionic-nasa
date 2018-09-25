8c8314c033c49922ffa4a7e284864b74deabd509
regina besa <reginabesa@gmail.com>
2018-09-25 10:57:48 -0500

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ApodProvider } from '../../providers/apod/apod';
+import { Apod } from '../../apod';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  apod: Apod;
 
+  constructor(
+    public navCtrl: NavController,
+    private apodProvider: ApodProvider
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.getApod();
+  }
+
+  randomDate(start, end){
+    let date = new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    );
+
+    let d:any = date.getDate();
+    let m:any = date.getMonth() + 1;
+    let y:any = date.getFullYear();
+
+    if(m<10){
+      m = '0'+m;
+    }
+
+    if(d<10){
+      d = '0'+d;
+    }
+
+    return y + '-' + m + '-' + d;
+  }
+
+  getApod(date:string=''):void {
+    this.apodProvider.getApod(date)
+      .subscribe(apod => this.apod = apod);
+  }
+
+  randomApod(): void{
+    let date = this.randomDate(
+      new Date(1995, 5, 16),
+      new Date()
+    );
+    this.getApod(date);
   }
 
 }
